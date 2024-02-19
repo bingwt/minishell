91825bb19ef74502925fd1c8ec3ae132274f7120
Bing Wen Tan <btan@student.42singapore.sg>
2024-02-19 19:45:07 +0800

    Added debug mode differentiate from real bash

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 19:27:32 by btan              #+#    #+#             */
-/*   Updated: 2024/02/19 18:55:41 by xlow             ###   ########.fr       */
+/*   Updated: 2024/02/19 19:33:22 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char	*prompt;
 	char	*buffer;
@@ -23,7 +23,13 @@ int	main(void)
 	while (1)
 	{
 		prompt = init_prompt();
-		buffer = readline(prompt);
+		if (argc == 2)
+		{
+			printf("%s mode\n", argv[1]);
+			buffer = readline("minibing> ");
+		}
+		else
+			buffer = readline(prompt);
 		free(prompt);
 		add_history(buffer);
 		if (!buffer)
