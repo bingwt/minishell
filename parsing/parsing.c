ace364ff619e554edc53c485310fd910f722eea7
xlow <lowxf7@gmail.com>
2024-02-22 05:00:30 +0800

    added main body of splitbyspace, comments on unimplemented logic

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -20,9 +20,15 @@ int		quotes(char c)
 	if (c == '\0')
 	{
 		if (squote)
+		{
+			squote = !squote;
 			return (1);
+		}
 		if (dquote)
+		{
+			dquote = !dquote;
 			return (2);
+		}
 	}
 	if (c == '\'' && !dquote)
 		squote = !squote;
@@ -35,21 +41,55 @@ char	**split_by_space(char *input)
 {
 	int		i;
 	int		j;
-	char	c;
+	int		cmd_idx;
 	char	*cmd;
 	char	**space_split;
 
 	i = 0;
 	j = 0;
+	cmd_idx = 0;
 	cmd = ft_calloc(1, 1);
+	space_split = NULL;
 	while (input[i])
 	{
-		c = input[i];
-		quotes(c);
-		if (c == ' ')
+		quotes(input[i]); // find a way print literal quotes
+		if (input[i] == ' ')
 		{
-			if (!quotes(c) && j > 0)
+			if (!quotes(input[i]) && j > 0)
 			{
+				space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
+				space_split[cmd_idx++] = cmd;
+				cmd = ft_calloc(1, 1);
+				j = 0;
+			}
+			else
+			{
+				cmd = ft_realloc(cmd, j + 1, j + 2); // pls test dis
+				cmd[j] = input[i];
+			}
+		}
+		else // control with else ifs and else after settling quotes
+		{
+			cmd = ft_realloc(cmd, j + 1, j + 2);
+			cmd[j] = input[i];
+		}
+	}
+	if (j)
+	{
+		space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
+		space_split[cmd_idx++] = cmd;
+	}
+	else
+		free(cmd);
+	space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
+	space_split[cmd_idx] = NULL;
+	if (quotes('\0'))
+	{
+		ft_free_split(&space_split);
+		perror("quotes unclosed");
+		return (NULL);
+	}
+	return (space_split);
 }
 
 char	**input_parser(char *input)
