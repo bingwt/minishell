1a509706727d785dbe830e4cfde75006c6aec0dd
x-low <xlow@c2r5s7.42singapore.sg>
2024-02-22 23:05:05 +0800

    Added flag for quotes to reset after each input

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,99 +6,131 @@
 /*   By: xlow <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:47:50 by xlow              #+#    #+#             */
-/*   Updated: 2024/02/21 21:12:46 by xlow             ###   ########.fr       */
+/*   Updated: 2024/02/22 22:08:30 by xlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int		quotes(char c)
+int	quotes(char c, char *reset)
 {
 	static int	squote = 0;
 	static int	dquote = 0;
 
-	if (c == '\0')
+	if (!reset)
 	{
-		if (squote)
+		if (c == '\0')
 		{
+			if (squote)
+				return (1);
+			if (dquote)
+				return (2);
+		}
+		if (c == '\'' && !dquote)
 			squote = !squote;
-			return (1);
-		}
-		if (dquote)
-		{
+		if (c == '\"' && !squote)
 			dquote = !dquote;
-			return (2);
-		}
+		return (squote | dquote);
 	}
-	if (c == '\'' && !dquote)
-		squote = !squote;
-	if (c == '\"' && !squote)
-		dquote = !dquote;
-	return (squote | dquote);
+	else if (squote || dquote)
+	{
+		squote = 0;
+		dquote = 0;
+		return (1);
+	}
+	return (0);
 }
 
 char	**split_by_space(char *input)
 {
-	int		i;
+	int		q;
 	int		j;
 	int		cmd_idx;
 	char	*cmd;
-	char	**space_split;
+	char	**split;
 
-	i = 0;
+	q = 0;
 	j = 0;
 	cmd_idx = 0;
 	cmd = ft_calloc(1, 1);
-	space_split = NULL;
-	while (input[i])
+	split = NULL;
+	while (*input)
 	{
-		quotes(input[i]); // find a way print literal quotes
-		if (input[i] == ' ')
+		if (*input == '\'' || *input == '\"')
 		{
-			if (!quotes(input[i]) && j > 0)
+			q = quotes(*input, NULL); // find a way print literal quotes
+			if ((quotes('\0', NULL) == 1 && *input == '\"')
+				|| (quotes('\0', NULL) == 2 && *input == '\''))
 			{
-				space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
-				space_split[cmd_idx++] = cmd;
+				cmd = ft_realloc(cmd, j + 1, j + 2);
+				cmd[j++] = *input;
+			}
+		}
+		else if (*input == ' ')
+		{
+			if (!quotes(*input,) && j > 0)
+			{
+				split = ft_realloc(split, cmd_idx * 8, (cmd_idx + 1) * 8);
+				split[cmd_idx++] = cmd;
 				cmd = ft_calloc(1, 1);
 				j = 0;
 			}
 			else
 			{
 				cmd = ft_realloc(cmd, j + 1, j + 2); // pls test dis
-				cmd[j] = input[i];
+				cmd[j++] = *input;
 			}
 		}
 		else // control with else ifs and else after settling quotes
 		{
 			cmd = ft_realloc(cmd, j + 1, j + 2);
-			cmd[j] = input[i];
+			cmd[j++] = *input;
 		}
 	}
 	if (j)
 	{
-		space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
-		space_split[cmd_idx++] = cmd;
+		split = ft_realloc(split, cmd_idx * 8, (cmd_idx + 1) * 8);
+		split[cmd_idx++] = cmd;
 	}
 	else
 		free(cmd);
-	space_split = ft_realloc(space_split, cmd_idx * 8, (cmd_idx + 1) * 8);
-	space_split[cmd_idx] = NULL;
+	split = ft_realloc(split, cmd_idx * 8, (cmd_idx + 1) * 8);
+	split[cmd_idx] = NULL;
 	if (quotes('\0'))
 	{
-		ft_free_split(&space_split);
+		ft_free_split(&split);
 		perror("quotes unclosed");
 		return (NULL);
 	}
-	return (space_split);
+	return (split);
 }
 
+int	main(int argc, char **argv)
+{
+	(void)	argc;
+	char	**s;
+	int		i = 0;
+
+	s = split_by_space(argv[1]);
+	while (s[i])
+	{
+		printf("Str %d: %s\n", i, s[i]);
+		i++;
+	}
+	ft_free_split(&s);
+	return (0);
+}
+/*
 char	**input_parser(char *input)
 {
+	char	*in;
 	char	**space_split;
 	char	**pipe_split;
 
 	if (!input)
 		return (NULL);
-	space_split = split_by_space(input);
+	in = input;
+	space_split = split_by_space(in);
 	pipe_split = split_by_pipes(space_split);
 }
+*/
