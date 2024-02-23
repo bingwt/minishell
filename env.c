47c143bf3559f394b56aa111c47b8dbd87308cc6
Bing Wen Tan <btan@student.42singapore.sg>
2024-02-23 16:16:01 +0800

    Added USER, HOME, DBUS_SESSION_BUS_ADDRESS and OLDPWD environment variables

--- env.c
+++ env.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 08:03:20 by btan              #+#    #+#             */
-/*   Updated: 2024/02/23 08:21:06 by btan             ###   ########.fr       */
+/*   Updated: 2024/02/23 15:59:28 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ char	**init_env(void)
 {
 	char	**env;
 
-	env = ft_calloc(4, sizeof(char *));
-	env[0] = ft_strjoin("PWD=", getenv("PWD"));
-	env[1] = ft_strjoin("TERM=", getenv("TERM"));
-	env[2] = ft_strjoin("DISPLAY=", getenv("DISPLAY"));
+	env = ft_calloc(8, sizeof(char *));
+	env[0] = ft_strjoin("USER=", getenv("USER"));
+	env[1] = ft_strjoin("HOME=", getenv("HOME"));
+	env[2] = ft_strjoin("PWD=", getenv("PWD"));
+	env[3] = ft_strjoin("DISPLAY=", getenv("DISPLAY"));
+	env[4] = ft_strjoin("TERM=", getenv("TERM"));
+	env[5] = ft_strjoin("DBUS_SESSION_BUS_ADDRESS=", \
+	getenv("DBUS_SESSION_BUS_ADDRESS"));
+	env[6] = ft_strjoin("OLDPWD=", getenv("OLDPWD"));
 	return (env);
 }
