af222500b4e4bfe79bffee6a5fc87403bc34f0a9
xlow <lowxf7@gmail.com>
2024-02-26 04:37:47 +0800

    inject_space error handling for unexpected token nl && extra spaces between redir

--- parsing/inject_space.c
+++ parsing/inject_space.c
@@ -2,16 +2,32 @@
 
 int	valid_redir(char *input)
 {
+	int	i;
+
+	i = 1;
 	if (!ft_strncmp(input, "<<<", 3) || !ft_strncmp(input, ">>>", 3))
 		return (0);
 	if (!ft_strncmp(input, "<>", 2) || !ft_strncmp(input, "><", 2))
+		return (0);
+	while (input[i] == ' ')
+		i++;
+	if (i > 1 && (input[i] == '<' || input[i] == '>'))
+		return (0);
+	if (input[i] == '\0')
 		return (0);
 	return (1);
 }
 
 void	assign_meta(t_string *res, int q, char curr, char next)
 {
-	if (!q && (next != '<' || next != '>'))
+	if (!q && (curr == '<' || curr == '>' || curr == '|')
+		&& next != '<' && next != '>')
+	{
+		cmd_assign(res, curr);
+		cmd_assign(res, ' ');
+	}
+	else if (!q && curr != '<' && curr != '>' && curr != '|'
+		&& (next == '<' || next == '>' || next == '|'))
 	{
 		cmd_assign(res, curr);
 		cmd_assign(res, ' ');
@@ -26,24 +42,21 @@ char	*inject_space(char *input)
 	int			q;
 	t_string	res;
 
-	i = 0;
+	i = -1;
 	res.i = 0;
 	res.s = ft_calloc(1, 1);
-	while (input[i])
+	while (input[++i])
 	{
 		q = quotes(input[i], NULL);
-		if (input[i] == '<' || input[i] == '>' || input[i] == '|')
+		if (input[i] == '<' || input[i] == '>')
 		{
 			if (!valid_redir(input + i))
 			{
 				free(res.s);
 				return (NULL);
 			}
-			assign_meta(&res, q, input[i], input[i + 1]);
 		}
-		else
-			cmd_assign(&res, input[i]);
-		i++;
+		assign_meta(&res, q, input[i], input[i + 1]);
 	}
 	quotes(input[i], "reset");
 	return (res.s);
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -19,7 +19,8 @@ void	split_by_space(char *input, char ***split)
 				|| (quotes('\0', NULL) == 2 && *input == '\''))
 				cmd_assign(&cmd, *input);
 		}
-		else if (*input == ' ')
+		else if (*input == ' ' || *input == '|'
+				|| *input == '>' || *input == '<')
 			handle_space(&cmd, split, &cmd_idx, q);
 		else
 			cmd_assign(&cmd, *input);
@@ -28,6 +29,19 @@ void	split_by_space(char *input, char ***split)
 	cleanup_space(&cmd, split, &cmd_idx);
 }
 
+/*
+char	**split_by_pipe(char **cmds)
+{
+	int	i;
+	int	cmd_idx;
+
+	if (valid_pipes(cmds))
+	{
+	}
+	ft_free_split(&cmds);
+*/
+
+/*
 char	**input_parser(char *input)
 {
 	char	*in;
@@ -38,6 +52,15 @@ char	**input_parser(char *input)
 		return (NULL);
 	in = input;
 	space_split = NULL;
+	in = inject_spaces(input);
+	if (!in)
+		return (NULL);
 	split_by_space(in, &space_split);
+	free(in);
+	if (!space_split)
+		return (NULL);
 	pipe_split = split_by_pipes(space_split);
-}
+	ft_free_split(&space_split);
+	if (!pipe_split)
+		return (NULL);
+}*/
