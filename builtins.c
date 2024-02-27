6114bfe1c4d6033881411486c24f600a6581fea4
Bing Wen Tan <btan@student.42singapore.sg>
2024-02-28 02:09:28 +0800

    Added handler for ft_export() if environment variable already exists

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:25:03 by btan              #+#    #+#             */
-/*   Updated: 2024/02/27 14:50:15 by btan             ###   ########.fr       */
+/*   Updated: 2024/02/28 02:05:40 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,27 @@ void	ft_env(char **envp)
 void	ft_export(char *cmd, t_list **envll)
 {
 	t_list	*env;
-
-	if (ft_strchr(cmd, '='))
+	char	**args;
+	char	*content;
+	char	*token;
+	
+	env = *envll;
+	args = ft_split(cmd, '=');
+	token = args[0];
+	while (env)
+	{
+		content = (char *) env->content;
+		if (!ft_strncmp(token + 7, content, ft_strchr(content, '=') - content))
+			break ;
+		env = env->next;
+	}
+	free_strs(args);
+	if (env)
+	{
+		printf("Already exists!\n");
+		return ;
+	}
+	if (!env && ft_strchr(cmd, '='))
 	{
 		env = ft_lstnew(ft_strdup(cmd + 7));
 		ft_lstadd_back(envll, env);
