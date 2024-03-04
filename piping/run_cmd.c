f6ae632dbbc2db73592d315d4b33a2e3641d057b
xlow <lowxf7@gmail.com>
2024-03-05 03:39:56 +0800

    rewrote openfiles && runsingle

--- piping/run_cmd.c
+++ piping/run_cmd.c
@@ -34,20 +34,20 @@ t_arg	open_files(t_arg args)
 	while (i <= args.in_i)
 	{
 		if (!ft_strcmp(args.in[i++], "<"))
-			args.io[0] = open(args.in[i++], O_RDONLY);
+			args.io[0] = open(args.in[i], O_RDONLY);
 		else
 			args.io[0] = heredoc implementation;
 		if (args.io[0] == -1)
 			error with exit, failed open == cmd not executed;
+		i++;
 	}
-	i = 1;
+	i = 0;
 	while (i <= args.out_i)
 	{
-		if (!ft_strcmp(args.in[args.in_i - 1], "<<"))
-			args.io[1] = open(args.out[i], APPEND, 0644);
+		if (!ft_strcmp(args.out[i++], ">>"))
+			args.io[1] = open(args.out[i++], APPEND, 0644);
 		else
-			args.io[1] = open(args.out[i], TRUNC, 0644);
-		i += 2;
+			args.io[1] = open(args.out[i++], TRUNC, 0644);
 		if (args.io[1] == -1)
 			error with exit;
 	}
@@ -58,31 +58,48 @@ void	dup_pipes(t_arg args, int *pipe)
 {
 }
 
-void	run_single(t_arg args, char **envp, t_list *envll)
+void	run_single(t_arg *args, char **envp, t_list *envll)
 {
-	int		std[2];
 	char	*path;
-	// i am destroying stdin and stdout every time i do this
-	// but im hoping that because everything runs through here
-	// it would reset every time before we get to anything
-	// that could possibly break
-	args = open_files(args);
-	if (!args.cmd[0])
+	pid_t	pid;
+
+	args[0] = open_files(args[0]);
+	if (!args[0].cmd[0])
 		return ;
-	if (dup2(args.io[0], 0) < 0 || dup2(args.io[1], 1) < 0)
+	if (dup2(args[0].io[0], 0) < 0 || dup2(args[0].io[1], 1) < 0)
 		perror("dup2"), return ;
-	if (builtin_table(args, envp, envll))
+	if (builtin_table(args[0], envp, envll))
 		return ;
-	if (!access(args.cmd[0], X_OK))
-		execve(args.cmd[0], args.cmd, envp);
-	path = get_path(args.cmd[0]);
-	if (!path)
-		perror(path), return ;
-	execve(path, args.cmd, *envp);
-	perror(execve), return ;
+	pid = fork();
+	if (pid < 0)
+		perror("fork"), return ;
+	if (!pid)
+	{
+		if (!access(args[0].cmd[0], X_OK))
+			execve(args[0].cmd[0], args[0].cmd, envp);
+		path = get_path(args[0].cmd[0]);
+		if (!path)
+			perror("path"), exit(1) ;
+		execve(path, args[0].cmd, envp);
+		perror("execve"), free_args(args), exit(1) ;
+	}
 }
 
 void	run_cmds(t_arg *args, char **envp, t_list *envll)
 {
+	int		exit_status;
+	pid_t	pid;
+
 	if (args[0].last)
-		run_single(args[0], envp, envll);
+		run_single(args, envp, envll);
+	else
+	{
+		pid = fork();
+		if (pid < 0)
+			perror("fork"), return ;
+		if (!pid)
+			minishell_piping(args, envp, envll);
+		waitpid(pid, &exit_status, WNOHANG);
+		fancy schmancy errno function;
+	}
+}
