66af4e77d1e9e6a5788590ca9730e8169f1968a6
Bing Wen Tan <btan@student.42singapore.sg>
2024-03-08 16:50:15 +0800

    Removed here_doc_w_pipe()

--- builtins/ft_heredoc.c
+++ builtins/ft_heredoc.c
@@ -6,34 +6,11 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 18:23:35 by btan              #+#    #+#             */
-/*   Updated: 2024/03/05 16:01:41 by btan             ###   ########.fr       */
+/*   Updated: 2024/03/08 16:49:55 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	ft_heredoc_w_pipe(char	*eof)
-{
-	int		p_fd[2];
-	char	*buffer;
-
-	pipe(p_fd);
-	while (1)
-	{
-		buffer = readline("heredoc> ");
-		if (!strcmp(buffer, eof))
-			return ;
-		write(p_fd[1], buffer, ft_strlen(buffer));
-		write(p_fd[1], "\n", 1);
-		free(buffer);
-	}
-	close(p_fd[1]);
-	buffer = ft_calloc(PIPE_BUF + 1, sizeof(char));
-	while (read(p_fd[0], buffer, PIPE_BUF) > 0);
-	printf("%s", buffer);
-	free(buffer);
-	close(p_fd[0]);
-}
 
 void	ft_heredoc(char	*eof, int fd)
 {
