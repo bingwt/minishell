280ec96310e78ed489bf5dd4ec04e6d46f295989
Bing Wen Tan <btan@student.42singapore.sg>
2024-03-27 10:56:04 +0800

    Fixed ft_echo() spacing

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:58:55 by btan              #+#    #+#             */
-/*   Updated: 2024/03/22 16:08:20 by btan             ###   ########.fr       */
+/*   Updated: 2024/03/27 10:55:40 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_echo(char **args)
 	len = ft_strslen(args);
 	while (i < len)
 	{
-		if (i > 0)
+		if (i > 1)
 			write(1, " ", 1);
 		write(1, args[i], ft_strlen(args[i]));
 		i++;
