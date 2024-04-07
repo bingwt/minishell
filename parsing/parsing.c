fe0aca79217035cbfa0dd134bfcc005e2f14ee5c
x-low <xlow@c1r7s4.42singapore.sg>
2024-04-07 20:08:22 +0800

    toing

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 20:10:20 by xlow              #+#    #+#             */
-/*   Updated: 2024/04/04 17:33:35 by xlow             ###   ########.fr       */
+/*   Updated: 2024/04/07 19:42:47 by xlow             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,9 @@ void	split_by_space(char *input, char ***split)
 		if (*input == '\'' || *input == '\"')
 		{
 			q = quotes(*input, NULL);
-			if ((quotes('\0', NULL) == 1 && *input == '\"')
-				|| (quotes('\0', NULL) == 2 && *input == '\''))
+			if (!quotes('\0', NULL))
+				cmd_assign(&cmd, '\a');
+			if ((q == 1 && *input == '\"') || (q == 2 && *input == '\''))
 				cmd_assign(&cmd, *input);
 		}
 		else if (ft_iswhitespace(*input))
